30aaa050ec975e3e21c5aece89dfa356965ed5f9
SohamDhotre <sohamsoham16@gmail.com>
2025-12-06 18:26:06 +0530

    Add 1 new LeetCode solution(s)

--- Two Pointers/Easy/844.backspace-string-compare/844.backspace-string-compare.java
+++ Two Pointers/Easy/844.backspace-string-compare/844.backspace-string-compare.java
@@ -1,15 +1,30 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character>stackS=new Stack<>();
-        Stack<Character>stackT=new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(ch=='#' && !stackS.isEmpty()) stackS.pop();
-            else if(ch!='#') stackS.push(ch);
+        StringBuilder a=new StringBuilder(s.length());
+        StringBuilder b=new StringBuilder(t.length());
+        for(char p:s.toCharArray()){
+            if(p=='#'){
+                if(a.length()>0){
+                    a.deleteCharAt(a.length()-1);
+                }
+            }
+            else{
+                a.append(p);
+            }
+            
         }
-        for(char ch:t.toCharArray()){
-            if(ch=='#' && !stackT.isEmpty()) stackT.pop();
-            else if(ch!='#') stackT.push(ch);
+        for(char q:t.toCharArray()){
+            if(q=='#'){
+                if(b.length()>0){
+                    b.deleteCharAt(b.length()-1);
+                }
+            }
+            else{
+                b.append(q);
+            }
         }
-        return stackT.equals(stackS);
+        
+        return a.toString().equals(b.toString());
+        
     }
 }
