a295f6eee21611f18ac0deb2389eb12e73f777ae
SohamDhotre <sohamsoham16@gmail.com>
2025-12-10 09:56:03 +0530

    Add 1 new LeetCode solution(s)

--- Arrays/Medium/503.next-greater-element-ii/503.next-greater-element-ii.java
+++ Arrays/Medium/503.next-greater-element-ii/503.next-greater-element-ii.java
@@ -3,10 +3,11 @@ class Solution {
         Deque<Integer>st=new ArrayDeque<>();
         int n=nums.length;
         int []res=new int[n];
-        for(int i=(2*n)-1;i>=0;i--){
-            while(!st.isEmpty() && st.peek()<=nums[(i%n)]) st.pop();
-            res[(i%n)]=st.isEmpty()?-1:st.peek();
-            st.push(nums[(i%n)]);
+        Arrays.fill(res, -1);
+        for(int i=0;i<2*n;i++){
+            int idx = i % n;
+            while(!st.isEmpty() && nums[st.peek()]<nums[idx]) res[st.pop()]=nums[idx];
+            if(idx<n) st.push(idx);
         }
         return res;
     }
